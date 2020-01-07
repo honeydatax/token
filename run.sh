0dcf1b0534d0c52e16dd21d001885bbe926e3efb
lisbon <16432726+honeydatax@users.noreply.github.com>
2020-01-07 19:57:35 +0000

    simple samples example

---
+++ run.sh
@@ -0,0 +1,5 @@
+echo run.sh
+export CLASSPATH=".:/usr/local/lib/antlr-4.7.2-complete.jar:$CLASSPATH"
+alias antlr4='java -jar /usr/local/lib/antlr-4.7.2-complete.jar'
+alias grun='java org.antlr.v4.gui.TestRig'
+javac Expr*.java
