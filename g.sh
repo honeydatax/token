d6fecf519fe3680a86be77abaa68740211791c30
lisbon <16432726+honeydatax@users.noreply.github.com>
2020-01-08 13:06:56 +0000

    delete g.h

--- g.sh
+++
@@ -1,6 +0,0 @@
-echo run.sh
-export CLASSPATH=".:/usr/local/lib/antlr-4.7.2-complete.jar:$CLASSPATH"
-alias antlr4='java -jar /usr/local/lib/antlr-4.7.2-complete.jar'
-alias grun='java org.antlr.v4.gui.TestRig'
-echo $grun
-grun Expr prog -gui < out.txt
