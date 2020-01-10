989c81368191003a361c870bb7789c8f0e95e48a
lisbon <16432726+honeydatax@users.noreply.github.com>
2020-01-10 09:50:47 +0000

    end of line token

---
+++ line.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+	char tokens[82];
+	char *p1;
+	char *p2;
+	int b=0;
+	printf("input token?: ");
+	p1=fgets(tokens,80,stdin);
+	p2=strstr(p1," ");
+	if(p2!=NULL){
+		p2[0]='!';
+		while(b!=1){
+				p2=strstr(p1,",");
+				if(p2!=NULL){
+					p2[0]='!';
+				}else{
+					b=1;
+				}
+		}
+	}
+	p2=strstr(p1,"\n");
+	if(p2!=NULL)p2[0]='!';
+	p2=strstr(p1,"\r");
+	if(p2!=NULL)p2[0]='!';
+	printf("\n%s\n",p1);
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
