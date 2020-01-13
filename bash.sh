243a4af75d05dbce44f62787715aa6f98cd91f2a
lisbon <16432726+honeydatax@users.noreply.github.com>
2020-01-13 12:42:37 +0000

    list file to install gcc

---
+++ bash.sh
@@ -0,0 +1,23 @@
+# auto install for gcc
+sudo apt-get install libc-dev
+sudo apt-get install libc6-dev
+sudo apt-get install gcc-multilib
+sudo apt-get install make
+sudo apt-get install make-guile
+sudo apt-get install manpages-dev
+sudo apt-get install autoconf
+sudo apt-get install automake
+sudo apt-get install libtool
+sudo apt-get install flex
+sudo apt-get install bison
+sudo apt-get install gdb
+sudo apt-get install gdb-minimal
+sudo apt-get install gdb-python2
+sudo apt-get install gcc-doc
+sudo apt-get install cpp
+sudo apt-get install gcc
+sudo apt-get install gcc-6
+
+
+
+
