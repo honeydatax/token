11e0999f0ea7ab047da96ea8358ca05235a46095
lisbon <16432726+honeydatax@users.noreply.github.com>
2020-01-14 12:21:43 +0000

    big companies  and projects team works

---
+++ to_do.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void to_do_task();
+void get_input1();
+void get_input2();
+void get_solvingProblemFunctionA();
+void get_solvingProblemFunctionB();
+void printing_solving();
+
+int main(){
+	//and me will mount gest you tasks groups
+	to_do_task();
+	get_input1();
+	get_input2();
+	get_solvingProblemFunctionA();
+	get_solvingProblemFunctionB();
+	printing_solving();
+	return 0;
+	
+}
+
+void to_do_task(){
+	//MIKE will do and test this function
+	//report mike do this function
+	printf("to do task () test by MIKE\n");
+
+}
+
+void get_input1(){
+	//JIM will do and test this function
+	//report JIM do this function
+	printf("get input1 () test by JIM\n");
+}
+
+void get_input2(){
+	//SPIKY will do and test this function
+	//report SPIKY do this function
+	printf("get input2 () test by SPIKY\n");
+}
+
+void get_solvingProblemFunctionA(){
+	//BILL will do and test this function
+	//report BILL do this function
+	printf("void get solving Problem FunctionA() test by BILL\n");
+}
+
+
+void get_solvingProblemFunctionB(){
+	//JIM will do and test this function
+	//report JIM do this function
+	printf("void get solving Problem FunctionB() test by JIM\n");
+}
+
+
+void printing_solving(){
+	//RICK will do and test this function
+	//report RICK do this function
+	printf("printing solved several senaries() test by RICK\n");
+}
+
+
+
+
+
+
+
+
